ceb9da930f888c9ddafdadb5900cfa7cbaf910aa
haesler-oscar-emf <143510179+haesler-oscar-emf@users.noreply.github.com>
2025-02-26 09:12:31 +0100

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "https://webapp.oscarhaesler.emf-informatique.ch/";
+const apiEndpoint = "https://app-210-300721-haesler-oscar.mangodesert-bd5a715a.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
